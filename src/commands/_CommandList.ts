5636867539deb7e6b6d5c2a58a4bcd41e6768a59
Nandagopal Menon <71086360+nandu-not-bot@users.noreply.github.com>
2022-04-26 11:36:29 +0530

    Added sum command

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,4 +1,5 @@
 import { Command } from "../interfaces/Command";
 import { hello } from "./hello";
+import { sum } from "./sum";
 
-export const CommandList: Command[] = [hello]
+export const CommandList: Command[] = [hello, sum]
---
+++ src/commands/sum.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder, SlashCommandIntegerOption } from "@discordjs/builders";
+import { Command } from "../interfaces/Command";
+
+export const sum: Command = {
+    data: new SlashCommandBuilder()
+        .setName("sum")
+        .setDescription("Sum two numbers")
+        .addIntegerOption(
+            new SlashCommandIntegerOption()
+                .setName("a")
+                .setDescription("The first number")
+                .setRequired(true)
+    )
+        .addIntegerOption(
+            new SlashCommandIntegerOption()
+                .setName("b")
+                .setDescription("The second number")
+                .setRequired(true)
+    ),
+    run: async (interaction) => { 
+        const a = interaction.options.getInteger("a", true)
+        const b = interaction.options.getInteger("b", true)
+
+        interaction.reply(`The sum of \`${a}\` and \`${b}\` is \`${a + b}\``);
+    }
+}
