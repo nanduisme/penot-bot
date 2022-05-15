208f8ec7d7387b03c15d99109594c1ed19b9c402
Nandagopal Menon <71086360+nandu-not-bot@users.noreply.github.com>
2022-05-15 13:24:50 +0530

    Wrap up

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,9 +1,9 @@
 import { Command } from "../interfaces/Command";
+import { help } from "./help";
 import { leavegames } from "./leavegames";
 import { math } from "./math";
 import { ping } from "./ping";
 import { rps } from "./rockPaperScissors";
-import { test } from "./test";
 import { xo } from "./xo";
 
-export const CommandList: Command[] = [math, rps, ping, leavegames, xo, test];
+export const CommandList: Command[] = [math, rps, ping, leavegames, xo, help];
